4c042a7e32f583e4aaaf7c05037a0aba3b0e760b
cleentfaar <leentfaar87@gmail.com>
2013-10-26 19:34:30 +0200

    First commit

---
+++ CleentfaarThreeJsBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the CleentfaarSelect2Bundle package.
+ *
+ * (c) Cas Leentfaar <http://cleentfaar.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+namespace Cleentfaar\Bundle\ThreeJsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CleentfaarThreeJsBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+CleentfaarBootstrapBundle
+=========================
+
+Symfony2 Bundle for integrating the Three.js 3D engine for HTML5 games
